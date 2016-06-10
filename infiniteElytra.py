6a0b8335172372766397de84f7794738c44bd0f8
MochifishCoder <joaquinww8@gmail.com>
2016-06-09 18:53:00 -0700

    Create infiniteElytra.py

---
+++ infiniteElytra.py
@@ -0,0 +1,44 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+import random
+import time
+
+# flying flag
+flying = False
+start = 0
+
+mc.postToChat("Go to y = 80 or above to fly!")
+# main loop (infinite)
+while True:
+
+    # get the player's position
+    pos = mc.player.getPos()
+
+    x = pos.x
+    y = pos.y
+    z = pos.z
+
+    # should we enable flying?
+    if flying == False and y >= 80:
+        mc.postToChat("Now Flying! Go to y = 80 or below to stop!")
+        flying = True
+        # remember when we started flying
+        start = time.time()
+
+    # are we flying?
+    if flying == True:
+        # is it time to move the player?
+        if time.time() - start > 15:
+        x = 8402
+        y = 256
+        z = 1158
+            mc.player.setTilePos(x, y, z)
+
+            # reset the timer
+            start = time.time()
+
+        # time to stop flying?
+        elif y <= 80:
+            mc.postToChat("No Longer Flying! Go to y = 80 or above to fly again!")
+            flying = False
