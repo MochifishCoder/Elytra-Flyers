1e77de76700360e8640173706055696d747a240e
MochifishCoder <joaquinww8@gmail.com>
2016-06-23 09:00:38 -0700

    Remove Unnecessary Code, More v2.1 Work

--- elytra.py
+++ elytra.py
@@ -1,9 +1,13 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-import random
+mc.postToChat("Go to y = 100 or above to fly!")
 
-x = 43
-y = 256
-z = 765
-mc.player.setTilePos(x, y, z)
+if y >= 100:
+  mc.player.getPos()
+  
+  x = pos.x
+  y = 512
+  z = pos.y
+  
+  mc.player.setTilePos(x, y, z)
