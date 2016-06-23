c0dcf7c5b0ebcf45373007546e24cf09bb7375d5
MochifishCoder <joaquinww8@gmail.com>
2016-06-23 10:46:01 -0700

    Added Position Checking

--- elytraRandom.py
+++ elytraRandom.py
@@ -5,9 +5,15 @@ import random
 
 mc.postToChat("Go to y = 100 or above to fly!")
 
-if y >= 100:
-  x = random.randint(1, 10000)
-  y = random.randint(256, 512)
-  z = random.randint(1, 10000)
+while True:
+  
+  mc.player.getPos()
 
-  mc.player.setTilePos(x, y, z)
+  if y >= 100:
+    x = random.randint(1, 10000)
+    y = random.randint(256, 512)
+    z = random.randint(1, 10000)
+
+    mc.player.setTilePos(x, y, z)
+    
+    break
