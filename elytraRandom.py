6a4c1079b5787439d92c3d51554136129db6990a
MochifishCoder <joaquinww8@gmail.com>
2016-06-23 08:44:02 -0700

    Fix More Imbalances, Start of 2.1 Update

--- elytraRandom.py
+++ elytraRandom.py
@@ -4,7 +4,7 @@ mc = Minecraft.create()
 import random
 
 x = random.randint(1, 10000)
-y = random.randint(80, 256)
+y = random.randint(256, 512)
 z = random.randint(1, 10000)
 
 mc.player.setTilePos(x, y, z)
