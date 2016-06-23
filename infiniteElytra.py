6805c39b0b58e7b2810fe419b93453818c538194
MochifishCoder <joaquinww8@gmail.com>
2016-06-22 20:17:25 -0700

    Incorrect Instructions Bug Fixed

--- infiniteElytra.py
+++ infiniteElytra.py
@@ -7,7 +7,7 @@ import time
 flying = False
 start = 0
 
-mc.postToChat("Go to y = 80 or above to fly!")
+mc.postToChat("Go to y = 100 or above to fly!")
 # main loop (infinite)
 while True:
 
@@ -20,7 +20,7 @@ while True:
 
     # should we enable flying?
     if flying == False and y >= 100:
-        mc.postToChat("Now Flying! Go to y = 80 or below to stop!")
+        mc.postToChat("Now Flying! Go to y = 99 or below to stop!")
         flying = True
         # remember when we started flying
         start = time.time()
@@ -39,5 +39,5 @@ while True:
 
         # time to stop flying?
         elif y <= 99:
-            mc.postToChat("No Longer Flying! Go to y = 80 or above to fly again!")
+            mc.postToChat("No Longer Flying! Go to y = 100 or above to fly again!")
             flying = False
